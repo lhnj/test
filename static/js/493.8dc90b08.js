65a79e3dd308dad15b03f14ac3199e49b60b6292
lhnj <2431563625@qq.com>
2024-02-21 18:56:31 +0800

    1

---
+++ static/js/493.8dc90b08.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkfront"]=self["webpackChunkfront"]||[]).push([[493],{6493:function(n,t,u){u.r(t),u.d(t,{default:function(){return f}});var s=function(){var n=this;n._self._c;return n._m(0)},r=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],e=u(1001),a={},l=(0,e.Z)(a,s,r,!1,null,null,null),f=l.exports}}]);
+//# sourceMappingURL=493.8dc90b08.js.map
